08594760502100289ce08a8e47e884f5afa73083
AbdulazizSaud <80a.tutorial@gmail.com>
2016-11-03 18:37:11 +0300

    Hi

--- app/src/main/java/com/example/kay/hoplay/MainActivity.java
+++ app/src/main/java/com/example/kay/hoplay/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
     //xxs
     //xx..
     // imprefect :)
+    // luffy con
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
