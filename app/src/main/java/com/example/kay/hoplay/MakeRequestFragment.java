6e4c2eea0d63133e697c6a66aaa818cf04e5de39
AbdulazizSaud <80a.tutorial@gmail.com>
2016-11-11 22:05:10 +0300

    new request class . . .

--- app/src/main/java/com/example/kay/hoplay/MakeRequestFragment.java
+++ app/src/main/java/com/example/kay/hoplay/MakeRequestFragment.java
@@ -1,5 +1,6 @@
 package com.example.kay.hoplay;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
@@ -9,23 +10,17 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MakeRequestFragment extends Fragment {
 
-
-    private  TextView makeRequestMessage ;
-    private Button makeRequestButton ;
-    private Spinner regionSpinner ;
-    private Spinner numberOfPlayersSpinner ;
-    private Spinner playersRanksSpinner;
-    private RadioButton pcRadiobutton ;
-    private RadioButton psRadiobutton;
-    private RadioButton xboxRadiobutton;
+    private  Button newRequestButton;
 
     public MakeRequestFragment() {
         // Required empty public constructor
@@ -37,60 +32,18 @@ public class MakeRequestFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_make_request_fragment, container, false);
-        Typeface sansationbold = Typeface.createFromAsset(getResources().getAssets(), "sansationbold.ttf");
-        makeRequestMessage = (TextView) view.findViewById(R.id.make_request_message_textview);
-        makeRequestMessage.setTypeface(sansationbold);
-        makeRequestButton = (Button) view.findViewById(R.id.make_request_button);
-        makeRequestButton.setTypeface(sansationbold);
-        regionSpinner = (Spinner) view.findViewById(R.id.region_spinner);
-        numberOfPlayersSpinner = (Spinner) view.findViewById(R.id.number_of_players_spinner);
-        playersRanksSpinner = (Spinner) view.findViewById(R.id.players_rank_spinner);
-        pcRadiobutton = (RadioButton) view.findViewById(R.id.pc_choice_radiobutton);
-        pcRadiobutton.setTypeface(sansationbold);
-        psRadiobutton = (RadioButton) view.findViewById(R.id.ps_choice_radiobutton);
-        psRadiobutton.setTypeface(sansationbold);
-        xboxRadiobutton = (RadioButton) view.findViewById(R.id.xbox_choice_radiobutton);
-        xboxRadiobutton.setTypeface(sansationbold);
-
-
-      /*  ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinnner_item, areay of values) {
-
-            public View getView(int position, View convertView,ViewGroup parent) {
-
-                View v = super.getView(position, convertView, parent);
-
-                ((TextView) v).setTextSize(16);
-
-                return v;
-
+        newRequestButton = (Button) view.findViewById(R.id.new_request_button);
+        newRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(),NewRequest.class);
+                startActivity(i);
             }
-
-            public View getDropDownView(int position, View convertView,ViewGroup parent) {
-
-                View v = super.getDropDownView(position, convertView,parent);
-
-                ((TextView) v).setGravity(Gravity.CENTER);
-
-                return v;
-
-            }
-
-        };
-        */
-
-        ArrayAdapter regionAdapter = ArrayAdapter.createFromResource(getContext() ,R.array.countries_array,R.layout.spinnner_item);
-        ArrayAdapter playersRanksAdapter = ArrayAdapter.createFromResource(getContext() ,R.array.players_ranks,R.layout.spinnner_item);
-        ArrayAdapter playersNumberAdapter = ArrayAdapter.createFromResource(getContext() ,R.array.players_number,R.layout.spinnner_item);
-
-        playersRanksAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        regionAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        playersNumberAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-
-        numberOfPlayersSpinner.setAdapter(playersNumberAdapter);
-        regionSpinner.setAdapter(regionAdapter);
-        playersRanksSpinner.setAdapter(playersRanksAdapter);
+        });
 
         return view;
     }
+
+
 
 }
