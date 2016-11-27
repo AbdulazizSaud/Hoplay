c79b93f2492728883262fc4f10087123e67b2282
azoz-pc <80a.tutorial@gmail.com>
2016-11-27 23:07:57 +0300

    user picture

--- app/src/main/java/com/example/kay/hoplay/Fragments/UserFragment.java
+++ app/src/main/java/com/example/kay/hoplay/Fragments/UserFragment.java
@@ -1,16 +1,25 @@
 package com.example.kay.hoplay.Fragments;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kay.hoplay.Adapters.CommunityAdapter;
 import com.example.kay.hoplay.Adapters.RecentActivitiesAdapter;
@@ -26,12 +35,17 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
 
+    private final static int RESULT_LOAD_IMG=1;
 
     private TextView usernameProfile;
     private TextView toUserGames;
@@ -43,8 +57,8 @@ public class UserFragment extends Fragment {
     private TextView followings_number;
     private TextView followers_number;
     private TextView recentActivitiesTextView;
-    private  TextView nickname;
-
+    private TextView nickname;
+    private CircleImageView userPicture;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -75,7 +89,7 @@ public class UserFragment extends Fragment {
         followers_number = (TextView) view.findViewById(R.id.followers_number_textview);
         recentActivitiesTextView = (TextView) view.findViewById(R.id.recent_activities_textview);
         nickname = (TextView) view.findViewById(R.id.nickname_profile_textView);
-
+        userPicture = (CircleImageView) view.findViewById(R.id.user_profile_photo_imageView);
 
         Typeface sansation = Typeface.createFromAsset(getActivity().getAssets() ,"sansationbold.ttf");
         usernameProfile.setTypeface(sansation);
@@ -160,6 +174,26 @@ public class UserFragment extends Fragment {
         });
 
 
+        userPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 new AlertDialog.Builder(v.getContext())
+                        .setTitle("Change Profile Picture")
+                        .setPositiveButton("Change Picture", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                // Start the Intent
+                                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                            }
+                        }).setNegativeButton("Change Cover",null)
+                        .show();
+
+
+            }
+        });
 
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recent_activities_recyclerview);
@@ -174,12 +208,6 @@ public class UserFragment extends Fragment {
 
         // specify an adapter (see also next example)
         ArrayList<RecentActivityList> recentActivityLists=new ArrayList<RecentActivityList>();
-
-
-
-
-
-
 
         recentActivityLists.add(addRecentActivity("Played Rocket League","http://www.mobygames.com/images/covers/l/307552-rocket-league-playstation-4-front-cover.jpg","Category : PC Games","15 min ago"));
         recentActivityLists.add(addRecentActivity("Played Overwatch","https://b.thumbs.redditmedia.com/ksN39DPM7HFaXTP_tBi-IuYYfWccRBjYykD6VFSePXE.jpg","Category : PC Games","24 min ago"));
@@ -208,6 +236,45 @@ public class UserFragment extends Fragment {
         recentActivity.setActivityDescription(activityDescription);
         recentActivity.setActivityDate(activityDate);
         return recentActivity;
+    }
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+
+                Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+
+                // Set the Image in ImageView after decoding the String
+                userPicture.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(getContext(), "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 
 
