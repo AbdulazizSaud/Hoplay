ae5a36d23f4b6f15ad62e9ad2254ebeab4516730
AbdulazizSaud <80a.tutorial@gmail.com>
2016-11-29 14:29:07 +0300

    New request v2

--- app/src/main/java/com/example/kay/hoplay/Activities/ChatActivity.java
+++ app/src/main/java/com/example/kay/hoplay/Activities/ChatActivity.java
@@ -234,10 +234,13 @@ public class ChatActivity extends ActionBarActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
+
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
             }
+
 
             @Override
             public void afterTextChanged(Editable s) {
